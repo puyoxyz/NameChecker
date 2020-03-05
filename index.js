df5a57d7e7d914ece6090f0837299df118eeaec6
daniel11420 <kiziolek06@gmail.com>
2020-03-05 06:03:53 +0000

    First working version. Probably final too, there's nothing else to add

--- index.js
+++ index.js
@@ -2,13 +2,30 @@
 let fs = require('fs');
 let readline = require('readline');
 let winston = require('winston');
+let https = require('https');
 
-// Create the Winston logger
+// Create the Winston loggers
 const logger = winston.createLogger({
   format: winston.format.simple(),
   transports: [
     new winston.transports.Console(),
     new winston.transports.File({ filename: 'app.log' })
+  ]
+});
+
+const takenLogger = winston.createLogger({
+  format: winston.format.simple(),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'taken.log' })
+  ]
+});
+
+const inappropiateLogger = winston.createLogger({
+  format: winston.format.simple(),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'inappropiate.log' })
   ]
 });
 
@@ -19,6 +36,28 @@ let names = [];
 
 function checkNames(names) {
   logger.info("Starting to check names");
+
+  names.forEach(function(name, index) {
+    https.get('https://auth.roblox.com/v1/usernames/validate?birthday=1967-03-01T23:00:00.000Z&context=Signup&username=' + name, (resp) => {
+      let data = '';
+
+      resp.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      resp.on('end', () => {
+        let code = JSON.parse(data).code;
+
+        if (code == "0") {
+          logger.info("[VALID] " + name);
+        } else if (code == "2") {
+          inappropiateLogger.info("[INAPP] " + name);
+        } else if (code == "1") {
+          takenLogger.info("[TAKEN] " + name);
+        }
+      });
+    })
+  });
 }
 
 logger.info("Reading Names file...");
